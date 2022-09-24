105b280d9d9c8a83863107a7fe934d317cbc231c
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-09-24 12:58:17 -0500

    animate choices

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var scissorsImg = document.getElementById('scissors')
 var choices = ["r", "p", "s", "a"]
 var humanScore = 0
 var botScore = 0
-
+var imageElements = [rockImg, paperImg, scissorsImg]
 
 rockImg.addEventListener('click', startRound);
 paperImg.addEventListener('click', startRound);
@@ -15,36 +15,39 @@ scissorsImg.addEventListener('click', startRound);
 
 function startRound(event) {
 
-  var humanChoice = event.target.dataset.letter
+    var humanChosenImg = event.target
+    var humanChoice = humanChosenImg.dataset.letter
 
 
-  // randomly choose computerChoice
-  var random = Math.floor(Math.random() * choices.length)
-  var computerChoice = choices[random]
+    // randomly choose computerChoice
+    var random = Math.floor(Math.random() * choices.length)
+    var computerChoice = choices[random]
+    document.querySelector('img[data-letter=" + computerChoice +"]')
 
 
-  var result
-  if (humanChoice === computerChoice) {
-    result = "Tied!"
-  } else if (
-    humanChoice === "r" && computerChoice === "s" ||
-    humanChoice === "p" && computerChoice === "r" ||
-    humanChoice === "s" && computerChoice === "p"
-  ) {
-    humanScore++
-    result = "You Won!"
-  } else {
-    botScore++
-    result = "Bot Won!"
-  }
+    var result
+    if (humanChoice === computerChoice) {
+        result = "Tied!"
+    } else if (
+        humanChoice === "r" && computerChoice === "s" ||
+        humanChoice === "p" && computerChoice === "r" ||
+        humanChoice === "s" && computerChoice === "p"
+    ) {
+        humanScore++
+        result = "You Won!"
+    } else {
+        botScore++
+        result = "Bot Won!"
+    }
+    // hide all three images
+   imageElements.forEach(function(img){
 
-//   // display (alert) comparison results (won, tied, lost round)
-//   alert("You " + result)
+    img.style.display = 'none'
+   })
+   humanChosenImg.style.display = "block"
+   computerChosenImg.style.display = "block"
 
-//   // show stats
-//   alert("Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties)
-
-};
+}
 
 
 
