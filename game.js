74e956f045f536b7b87d1f2b648be05cb3bee3ef
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-09-24 14:01:22 -0500

    working js

--- game.js
+++ game.js
@@ -1,54 +1,93 @@
 var humanScoreEl = document.getElementById('human-score')
 var botScoreEl = document.getElementById('bot-score')
-var rockImg = document.getElementById('rock')
-var paperImg = document.getElementById('paper')
-var scissorsImg = document.getElementById('scissors')
-var choices = ["r", "p", "s", "a"]
+var rockEl = document.getElementById('rock')
+var paperEl = document.getElementById('paper')
+var scissorsEl = document.getElementById('scissors')
+var choicesElements = [rockEl, paperEl, scissorsEl]
+var choices = ["r", "p", "s"]
 var humanScore = 0
 var botScore = 0
-var imageElements = [rockImg, paperImg, scissorsImg]
-
-rockImg.addEventListener('click', startRound);
-paperImg.addEventListener('click', startRound);
-scissorsImg.addEventListener('click', startRound);
+var resultEl = document.getElementById('result')
 
 
-function startRound(event) {
 
-    var humanChosenImg = event.target
-    var humanChoice = humanChosenImg.dataset.letter
+function resetUI(){
 
+    
+}
 
-    // randomly choose computerChoice
-    var random = Math.floor(Math.random() * choices.length)
-    var computerChoice = choices[random]
-    document.querySelector('img[data-letter=" + computerChoice +"]')
-
-
-    var result
-    if (humanChoice === computerChoice) {
-        result = "Tied!"
-    } else if (
-        humanChoice === "r" && computerChoice === "s" ||
-        humanChoice === "p" && computerChoice === "r" ||
-        humanChoice === "s" && computerChoice === "p"
-    ) {
-        humanScore++
-        result = "You Won!"
-    } else {
-        botScore++
-        result = "Bot Won!"
-    }
-    // hide all three images
-   imageElements.forEach(function(img){
-
-    img.style.display = 'none'
-   })
-   humanChosenImg.style.display = "block"
-   computerChosenImg.style.display = "block"
+function updateScores(){
+humanScoreEl.innerText=humanScore;
+botScoreEl.innerText=botScore;
 
 }
 
+
+
+function showResults(humanChosenEl,computerChosenEl,result){
+resultEl.innerText = result
+updateScores()
+
+    choicesElements.forEach(function(img) {
+        img.style.display = 'none'
+      })
+      humanChosenEl.style.display = "block"
+      computerChosenEl.style.display = "block"
+
+      var humanSpan = document.createElement('span')
+      humanSpan.innerHTML = "Human"
+      humanChosenEl.appendChild(humanSpan)
+
+      var botSpan = document.createElement('span')
+      botSpan.innerHTML = "Bot"
+      computerChosenEl.appendChild(botSpan)
+
+}
+
+
+function startRound(event) {
+  var humanChosenEl
+  // make sure we're referring to the correct element
+  if (event.target.matches('img')) {
+    humanChosenEl = event.target.parentElement
+  } else {
+    humanChosenEl = event.target
+  }
+
+  var humanChoice = humanChosenEl.dataset.letter
+  
+  // randomly choose computerChoice
+  var random = Math.floor(Math.random() * choices.length)
+  var computerChoice = choices[random]
+  var computerChosenEl = document.querySelector('[data-letter="'+ computerChoice +'"]')
+
+  var result
+  if (humanChoice === computerChoice) {
+    result = "TIED!"
+  } else if (
+    humanChoice === "r" && computerChoice === "s" ||
+    humanChoice === "p" && computerChoice === "r" ||
+    humanChoice === "s" && computerChoice === "p"
+  ) {
+    humanScore++
+    result = "YOU WON!"
+  } else {
+    botScore++
+    result = "BOT WON!"
+  }
+
+
+  console.log(humanChosenEl, computerChosenEl)
+
+  // hide all three images
+  
+
+showResults(humanChosenEl, computerChosenEl, result)
+}
+
+rockEl.addEventListener('click', startRound)
+paperEl.addEventListener('click', startRound)
+scissorsEl.addEventListener('click', startRound)
 
 
 
--- gameFIRST.js
+++ gameFIRST.js
@@ -1,62 +1,99 @@
-//allow user to select rock paper or scissors
+var humanScoreEl = document.getElementById('human-score')
+var botScoreEl = document.getElementById('bot-score')
+var rockEl = document.getElementById('rock')
+var paperEl = document.getElementById('paper')
+var scissorsEl = document.getElementById('scissors')
+var resultEl = document.getElementById('result')
 
-//computer selects randomly rock paper or scissors
+var choicesElements = [rockEl, paperEl, scissorsEl]
+var choices = ["r", "p", "s"]
+var humanScore = 0
+var botScore = 0
+var clickable = true
 
-//display computers choice
-
-//display user choice
-
-//compare user choice to computer choice and determine outcome
-
-//display outcome
-
-
-
-var array1 = ["r", "p", "s"];
-
-
-let userChoice = prompt("Select R, P, or S");
-let computerChoice = array1[Math.floor(Math.random()*array1.length)];
-
-
-console.log (userChoice);
-console.log (computerChoice);
-
-
-if (userChoice === computerChoice){
-
-    alert("Tie");
-
+function resetUI(){
+    clickable = true
+    resultEl.innerText = "Rock Paper Scissors"
+    choicesElements.forEach(function(el) {
+        el.style.display = 'block'
+        el.classList.remove('loser')
+        var spans = el.querySelectorAll('span')
+        for (var i=0; i < spans.length; i++){
+            spans[i].remove()
+        }
+      })
 }
 
-else if (userChoice === "r" && computerChoice === "p"){
-    
-    alert("Computer Wins")
+function updateScores() {
+  humanScoreEl.innerText = humanScore
+  botScoreEl.innerText = botScore
 }
 
-else if ( userChoice === "r" && computerChoice === 2){
-   
-    alert("User Wins")
+function showResults(humanChosenEl, computerChosenEl, result) {
+    clickable = false;
+  resultEl.innerText = result
+  updateScores()
+  // hide all three
+  choicesElements.forEach(function(el) {
+    el.style.display = 'none'
+  })
+  humanChosenEl.style.display = "block"
+  computerChosenEl.style.display = "block"
+
+  var humanSpan = document.createElement('span')
+  humanSpan.innerText = "You"
+  humanChosenEl.appendChild(humanSpan)
+
+  var botSpan = document.createElement('span')
+  botSpan.innerText = "Bot"
+  computerChosenEl.appendChild(botSpan)
+
+  if (result === 'YOU WON!') {
+    computerChosenEl.classList.add('loser')
+  } else if (result === 'BOT WON!') {
+    humanChosenEl.classList.add ('loser')
+  }
+  setTimeout(function(){
+    resetUI()
+  },3000)
 }
 
-else if (userChoice === 1 && computerChoice === 2){
-    
-    alert("Computer Wins")
+function startRound(event) {
+if (!clickable) return
+
+  var humanChosenEl
+  // make sure we're referring to the correct element
+  if (event.target.matches('img')) {
+    humanChosenEl = event.target.parentElement
+  } else {
+    humanChosenEl = event.target
+  }
+
+  var humanChoice = humanChosenEl.dataset.letter
+  
+  // randomly choose computerChoice
+  var random = Math.floor(Math.random() * choices.length)
+  var computerChoice = choices[random]
+  var computerChosenEl = document.querySelector('[data-letter="'+ computerChoice +'"]')
+
+  var result
+  if (humanChoice === computerChoice) {
+    result = "TIED!"
+  } else if (
+    humanChoice === "r" && computerChoice === "s" ||
+    humanChoice === "p" && computerChoice === "r" ||
+    humanChoice === "s" && computerChoice === "p"
+  ) {
+    humanScore++
+    result = "YOU WON!"
+  } else {
+    botScore++
+    result = "BOT WON!"
+  }
+
+  showResults(humanChosenEl, computerChosenEl, result)
 }
 
-else if (userChoice === 1 && computerChoice === "r") {
-    
-    alert("User Wins")
-}
-
-else if (userChoice === 2 && computerChoice === "r"){
-    
-    alert("Computer Wins")
-}
-
-else if ( userChoice === 2 && computerChoice === 1) {
-   
-    alert("User Wins")
-}
-
-
+rockEl.addEventListener('click', startRound)
+paperEl.addEventListener('click', startRound)
+scissorsEl.addEventListener('click', startRound)
